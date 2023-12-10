f108fdfa336596855db5d8ba7d4057e6539012a2
ATLCode <aleksanteri.leinonen@gmail.com>
2023-12-10 16:29:46 +0200

    Added vuetify and programmatic listing of projects

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,80 @@
 <template>
   <div class="home-container">
     <div class="introduction">test</div>
+    <div class="jobs">
+      <div class="jHeader">Experience</div>
+      <div class="jCard"></div>
+    </div>
     <div class="projects">
       <div class="pHeader">Projects</div>
-      <div class="pCard">
-        <div class="pImage"></div>
-        <div class="pInfo">
-          <div class="pName">Project Name</div>
-          <div class="pDescription">Project description</div>
+      <div class="pCard" v-for="project in projects" :key="project.name">
+        <div class="pImage">
+          <v-img :width="400" aspect-ratio="16/9" cover :src="project.img"></v-img>
         </div>
-        <div class="pTech">Tech</div>
+        <div class="pInfo">
+          <div class="pName">{{ project.name }}</div>
+          <div class="pDescription">{{ project.description }}</div>
+          <div class="pExtra"></div>
+        </div>
+        <div class="pTech">
+          <div class="pTechLogo" v-for="icon in project.tech" :key="icon">
+            <v-img
+              :width="100"
+              aspect-ratio="16/9"
+              cover
+              :src="`/src/assets/icons/${icon}.svg`"
+            ></v-img>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const projects = [
+  {
+    name: 'test',
+    description: 'asd',
+    img: '/src/assets/images/test.jpg',
+    tech: ['css', 'django'],
+    github: ''
+  },
+  {
+    name: 'test2',
+    description: 'asd2',
+    img: '/src/assets/images/test.jpg',
+    tech: ['git', 'github'],
+    github: ''
+  },
+  {
+    name: 'test3',
+    description: 'asd3',
+    img: '/src/assets/images/test.jpg',
+    tech: [],
+    github: ''
+  }
+]
+
+const jobs = [
+  {
+    title: 'Software Developer',
+    company: 'Onebyte Oy',
+    started: '2022',
+    finished: 'present',
+    description: '',
+    tech: []
+  },
+  {
+    title: 'Professional Coach',
+    company: 'Basketball & Esports',
+    started: '2012',
+    finished: 'present',
+    description: '',
+    tech: []
+  }
+]
+</script>
 
 <style scoped lang="scss">
 //General
@@ -37,6 +96,19 @@ img {
   height: 100vh;
   background-color: red;
 }
+.jobs {
+  min-height: 100vh;
+  background-color: blue;
+  display: flex;
+  flex-direction: column;
+  .jHeader {
+    background-color: yellow;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
 .projects {
   min-height: 100vh;
   background-color: blue;
@@ -57,10 +129,10 @@ img {
     gap: 1rem;
     .pImage {
       max-width: 100%;
-      display: flex;
+      display: block;
+      margin: auto;
       justify-content: center;
       align-items: center;
-      background-image: URL(../assets/images/test.jpg);
       background-position: center;
     }
     .pInfo {
@@ -75,6 +147,7 @@ img {
       }
     }
     .pTech {
+      display: flex;
     }
   }
 }
